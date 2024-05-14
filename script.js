67bc58fca13607fbb0581a4ff77e70b9d0614b87
Neil Doppler <neildoppler@Neils-Laptop.local>
2024-05-14 07:38:41 -0500

    addition of the random employee raffle function in the console

--- script.js
+++ script.js
@@ -47,7 +47,15 @@ console.log(`The average employee salary between our ${employeesArray.length} is
 
 // Select a random employee
 const getRandomEmployee = function (employeesArray) {
+
     // TODO: Select and display a random employee
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * employeesArray.length);
+      }
+    
+      let employeeRandomizer = getRandomInt()
+    
+    console.log(`Congratulations to ${employeesArray[employeeRandomizer].firstName} ${employeesArray[employeeRandomizer].lastName}, our random drawing winner!`);
 }
 
 /*
